9c7df3f718c8d864343d6db29560e740d0452c7e
Daulet Baimbetov <daucloudlab@gmail.com>
2018-08-29 14:37:36 +0600

    29_08_208_01

--- test1.py
+++ test1.py
@@ -1,35 +1,44 @@
-# with open('hello.txt', 'w') as f:
-#     f.write("Hello Python")
-#
-# f = open('hello.txt', 'w')
-# try:
-#     f.write('Hello Python2')
-# finally:
-#     f.close()
-# class ManagedFile:
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def __enter__(self):
-#         self.f = open('hello.txt', 'w')
-#         return self.f
-#
-#     def __exit__(self, ex_type, ex_val, ex_):
-#         if self.f:
-#             self.f.close()
-#
-#
-# with ManagedFile('hello.txt') as f:
-#     f.write("Hello Python3!")
-#     f.write("Hello again!2")
-# from contextlib import contextmanager
-# @contextmanager
-# def managed_file(name):
-#     try:
-#         f = open(name, 'w')
-#         yield f
-#     finally:
-#         f.close()
-# with managed_file('hello.txt') as f:
-#     f.write("Hello Python4!")
-#     f.write("Hello World!")
+# Context Manager-дің жұмысына мысал
+with open('hello.txt', 'w') as f:
+    f.write("Hello Python")
+
+# Context Manager ішінде осылай жұмыс жасайды
+f = open('hello.txt', 'w')
+try:
+    f.write('Hello Python2')
+finally:
+    f.close()
+
+# Біздің объектілеріміз де with операторымен жұмыс жасау үшін
+# __enter__ және __exit__  протоколдарын қолдану қажет
+class ManagedFile:
+    def __init__(self, name):
+        self.name = name
+
+    def __enter__(self):
+        self.f = open('hello.txt', 'w')
+        return self.f
+
+    def __exit__(self, ex_type, ex_val, ex_):
+        if self.f:
+            self.f.close()
+
+
+with ManagedFile('hello.txt') as f:
+    f.write("Hello Python3!")
+    f.write("Hello again!2")
+
+# contextlib модулінде contextmanager декораторы бар. Осы декораторды
+# қолдану арқылы функцияны with операторымен жұмыс жасай алатындай
+# етуге болады
+from contextlib import contextmanager
+@contextmanager
+def managed_file(name):
+    try:
+        f = open(name, 'w')
+        yield f
+    finally:
+        f.close()
+with managed_file('hello.txt') as f:
+    f.write("Hello Python4!")
+    f.write("Hello World!")
